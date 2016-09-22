72d140a1f5cc89491cc984a5cdd4108dc9459d33
zarque <zaonono@hotmail.fr>
2016-09-22 18:11:15 +0200

    Set download link

--- pages/html/home.php
+++ pages/html/home.php
@@ -114,7 +114,7 @@
     <div class="cta-content">
         <div class="container">
             <h2>Let's Get Started!</h2>
-            <a href="#github" class="btn btn-outline btn-xl page-scroll">Get the latest version</a>
+            <a href="https://github.com/zarque/PageManager/archive/master.zip" class="btn btn-outline btn-xl page-scroll">Get the latest version</a>
         </div>
     </div>
     <div class="overlay"></div>
@@ -133,7 +133,7 @@
         </div>
         <div class="row">
             <div class="col-md-12">
-              <p>First, <a href="">download the latest version</a> of PageManager, unzip the compressed file and place the content in your project folder, you will get something like that:
+              <p>First, <a href="https://github.com/zarque/PageManager/archive/master.zip">download the latest version</a> of PageManager, unzip the compressed file and place the content in your project folder, you will get something like that:
                 <figure class="highlight"><pre><code class="language-bash" data-lang="bash">PageManager/
   ├── assets/
   │   ├── css/
